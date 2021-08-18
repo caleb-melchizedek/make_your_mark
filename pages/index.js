a9a9271947e55da3758e8674e73d86133b55fa28
caleb melchizedek <caleb.melchizedek@acity.edu.gh>
2021-08-18 10:40:00 +0000

    landing page boiler plate

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,49 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+    <>
+    <Head>
+        <title>Make your Mark</title>
+        {/* <link rel="icon" href="" /> */}
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <nav>
+      </nav>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <header className="text-7xl self-center my-24 font-semibold ">
+        Make your mark
+      </header>
+      <button className="w-32 h-10 text-sm text-white font-semibold justify-self-end bg-red-400 rounded-3xl" >View featured</button>
     </div>
-  )
+
+    <main className="flex flex-col items-center justify-center w-full flex-1 px-4 text-center">
+      
+
+      <div className="divide-y divide-gray-400 w-full"> 
+          <h1 className="font-medium text-3xl " >Featured Marks</h1>
+
+        <section id="feature-marks" className="flex flex-col md:flex-row md:space-x-12 justify-evenly w-full pt-14">
+          {/* card */}
+          <div className="rounded-lg  md:w-64 my-12 md:my-0 p-4 relative shadow-xl  ">
+            <div className="rounded-full h-10 sm:h-16 w-10 sm:w-16 border-2 md:border-4 border-gray-700 shadow-xl object-right-top  absolute -top-5 -right-2">
+              <img src="/Mag_Mel-log.png" alt="Image" />
+            </div>
+            <h1 className="text-left font-semibold text-sm sm:text-2xl pb-4">Melchizedek</h1>
+            <p className="text-xs sm:text-sm text-left break-normal">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur laboriosam obcaecati ipsum sint ipsam maxime velit, eligendi amet debitis minus praesentium error ea quia temporibus odio quae unde perferendis cumque!
+            </p>
+          </div>
+
+        </section>
+      </div>
+
+    </main>
+
+    <footer className="flex items-center justify-center w-full h-24 border-t">
+      Powered by{" "}
+      <img src="/Mag_Mel-log.png" alt="Vercel Logo" className="h-7 ml-4" />
+    </footer>
+    </>
+  );
 }
